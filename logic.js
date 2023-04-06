80ba6909d0174680eb1a3c390b17b3c3c434aabe
Igme <88986288+igmeMarcial@users.noreply.github.com>
2023-04-05 20:52:29 -0500

    login

--- logic.js
+++ logic.js
@@ -20,36 +20,71 @@
 
 
 
-    function saltarLinea() {
-        document.write("<br>");    
-        document.write("<br>");    
-        document.write("<br>");    
-    }  
-    function imprimir(frase) {
-        document.write(frase);
-        saltarLinea();
-    }
-    var numeroPensado = Math.round(Math.random()*10);
-    var intentos = 3;
-    var contador = 1;
+    // function saltarLinea() {
+    //     document.write("<br>");    
+    //     document.write("<br>");    
+    //     document.write("<br>");    
+    // }  
+    // function imprimir(frase) {
+    //     document.write(frase);
+    //     saltarLinea();
+    // }
+    // var numeroPensado = Math.round(Math.random()*10);
+    // var intentos = 3;
+    // var contador = 1;
 
-    while (contador <= intentos) {
-        var numeroLanzado = parseInt(prompt("Ingrese un número entre 0-10"));
-        if (numeroPensado == numeroLanzado){
-            //corregir aquí
-            alert("usted acertó, en el intento " + contador + " el número pensado era " + numeroPensado);
-            break;
+    // while (contador <= intentos) {
+    //     var numeroLanzado = parseInt(prompt("Ingrese un número entre 0-10"));
+    //     if (numeroPensado == numeroLanzado){
+    //         //corregir aquí
+    //         alert("usted acertó, en el intento " + contador + " el número pensado era " + numeroPensado);
+    //         break;
+    //     }
+    //     else {
+    //         alert("usted erró");
+    //     }
+    //     contador++;
+    // }
+    // if (numeroPensado == numeroLanzado){
+    //     //corregir aquí
+    //     imprimir("usted acertó, en el intento " + contador + " el número pensado era " + numeroPensado);
+    // }
+    // else {
+    //     imprimir("usted erró, el número pensado era " + numeroPensado);
+    // }
+    // imprimir("fin");
+
+
+
+
+
+
+
+  
+
+    var inicioDeSesionRegistrado = "alura";
+    var contrasenhaRegistrada = "alura321";
+
+    var maximoIntentos = 3;
+    var intentoActual = 1;
+
+    while(intentoActual <= maximoIntentos){
+        var inicioDeSesionIngresado = prompt("Ingrese su usuario");
+        var contrasenhaIngresada = prompt("Ingrese su contraseña");
+
+        if(inicioDeSesionRegistrado == inicioDeSesionIngresado && contrasenhaRegistrada == contrasenhaIngresada){
+            alert("Bienvenido al sistema" + inicioDeSesionIngresado);
+            intentoActual = maximoIntentos; // Lo hice bien, así que paso todos los intentos que falten y salgo del loop. Allá abajo aumentará +1! 
+        } else {
+            if (intentoActual == 3) {
+                alert("Agotaste el número permitido de intentos!");
+            } else {
+                alert("Inicio de sesión inválido. Favor intente de nuevo");
+            }
         }
-        else {
-            alert("usted erró");
-        }
-        contador++;
+
+       // vaya al próximo intento 
+        intentoActual = intentoActual +1  
     }
-    if (numeroPensado == numeroLanzado){
-        //corregir aquí
-        imprimir("usted acertó, en el intento " + contador + " el número pensado era " + numeroPensado);
-    }
-    else {
-        imprimir("usted erró, el número pensado era " + numeroPensado);
-    }
-    imprimir("fin");
+
+
