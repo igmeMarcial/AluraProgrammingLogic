b2f48a6e9f517c41d6afeb81a972bea661dbc688
Igme <88986288+igmeMarcial@users.noreply.github.com>
2023-04-07 20:45:50 -0500

    Flat

--- canvas.js
+++ canvas.js
@@ -77,43 +77,205 @@
 //  no existe la implementación perfecta de un código. Lo importante de la
 //   función dibujarBarra es que cumpla con el objetivo propuesto.
 
-function dibujarRectangulo(x, y, base, altura, color) {
-    var pantalla = document.querySelector("canvas");
-    var pincel = pantalla.getContext("2d");
+// function dibujarRectangulo(x, y, base, altura, color) {
+//     var pantalla = document.querySelector("canvas");
+//     var pincel = pantalla.getContext("2d");
 
-    pincel.fillStyle=color;
-    pincel.fillRect(x,y, base, altura);
-    pincel.strokeStyle="black";
-    pincel.strokeRect(x,y, base, altura);
-}
+//     pincel.fillStyle=color;
+//     pincel.fillRect(x,y, base, altura);
+//     pincel.strokeStyle="black";
+//     pincel.strokeRect(x,y, base, altura);
+// }
 
-function escribirTexto(x , y, texto) {
-    var pantalla = document.querySelector("canvas");
-    var pincel = pantalla.getContext("2d");
+// function escribirTexto(x , y, texto) {
+//     var pantalla = document.querySelector("canvas");
+//     var pincel = pantalla.getContext("2d");
 
-    pincel.font="15px Georgia";
-    pincel.fillStyle="black";
-    pincel.fillText(texto, x, y);    
-}
+//     pincel.font="15px Georgia";
+//     pincel.fillStyle="black";
+//     pincel.fillText(texto, x, y);    
+// }
 
-function dibujarBarra(x, y, serie, colores, texto) {
+// function dibujarBarra(x, y, serie, colores, texto) {
 
-    escribirTexto(x, y - 10, texto);
+//     escribirTexto(x, y - 10, texto);
 
-    var sumaAltura = 0;
-    for (var i = 0; i < serie.length; i++) {
-        var altura = serie[i];
-        dibujarRectangulo(x, y + sumaAltura, 50, altura, colores[i]);
-        sumaAltura = sumaAltura + altura;
+//     var sumaAltura = 0;
+//     for (var i = 0; i < serie.length; i++) {
+//         var altura = serie[i];
+//         dibujarRectangulo(x, y + sumaAltura, 50, altura, colores[i]);
+//         sumaAltura = sumaAltura + altura;
+//     }
+// }
+
+// var colores = ["blue","green","yellow", "red","gray"];
+// var serie2009 = [6,47,41,3,3];
+// var serie2019 = [81,9,3,3,4];
+
+// dibujarBarra(50, 50, serie2009, colores, "2009");
+// dibujarBarra(150, 50, serie2019, colores, "2019");
+
+// Bueno el desafío es el siguiente, vamos a liberar que el usuario pueda alterar el 
+// color de los círculos que son diseñados en la pantalla. Los colores que liberaremos 
+// serán azul, rojo y verde (blue, red y green). Esa lista de colores te debe recordar
+//  algo que ya vimos, los arrays en el anterior curso.
+
+// var pantalla = document.querySelector("canvas");
+//     var pincel = pantalla.getContext("2d");    
+//     pincel.fillStyle = "grey";
+//     pincel.fillRect(0,0,600,400); 
+//     var colores = ["blue", "red", "green"];
+//     var indiceColorActual = 0; // comienza con blue
+
+//     function dibujarCirculo(evento){
+//         var x = evento.pageX - pantalla.offsetLeft;
+//         var y = evento.pageY - pantalla.offsetTop;    
+//         pincel.fillStyle = colores[indiceColorActual];
+//         pincel.beginPath();
+//         pincel.arc(x,y,10,0,2*3.14);
+//         pincel.fill();
+//         console.log(x + "," + y);        
+//     }
+
+//     pantalla.onclick = dibujarCirculo;
+
+//     function alterarColor() {
+//         indiceColorActual++;
+//         if(indiceColorActual>= colores.length) {
+//              indiceColorActual = 0; //vuelve para el primer color, blue
+//         }
+//         return false; //menú contextual padrón de `canvas` no sea exhibido
+//     }
+
+//     pantalla.oncontextmenu = alterarColor;
+
+
+    //
+    var pantalla = document.querySelector('canvas');
+    var pincel = pantalla.getContext('2d');
+
+    pincel.fillStyle = 'grey';
+    pincel.fillRect(0, 0, 600, 400);
+
+    var puedoDibujar = false;
+
+    //Variables para dibujar la paleta
+    var xRojo = 0;
+    var xVerde  = 50;
+    var xAzul = 100;
+    var yCuadrados = 0;
+    var tamanhoCuadrados = 50;
+    var colorActual = "blue";
+
+    function dibujarCirculo(x,y,colorActual ) {
+
+        if(puedoDibujar) {
+
+            pincel.fillStyle = colorActual;
+            pincel.beginPath();
+            pincel.arc(x, y, 5, 0, 2 * 3.14);
+            pincel.fill();
+
+        }
     }
-}
 
-var colores = ["blue","green","yellow", "red","gray"];
-var serie2009 = [6,47,41,3,3];
-var serie2019 = [81,9,3,3,4];
+    //Función para dibujar cada uno de los cuadrados de la paleta
+    function dibujarCuadrado(x, y, tamanho, color) {
 
-dibujarBarra(50, 50, serie2009, colores, "2009");
-dibujarBarra(150, 50, serie2019, colores, "2019");
+        pincel.fillStyle = color;
+        pincel.fillRect(x, y, tamanho, tamanho)
+        pincel.fill();
+
+    }
+
+    //Función para dibujar la paleta de colores
+    function dibujarPaletaColores() {
+
+        dibujarCuadrado(xRojo, yCuadrados, tamanhoCuadrados, 'red');
+        dibujarCuadrado(xVerde, yCuadrados, tamanhoCuadrados, 'green');
+        dibujarCuadrado(xAzul, yCuadrados, tamanhoCuadrados, 'blue');
+
+    }
+
+    function habilitarDibujar() {
+
+        puedoDibujar = true;
+
+    }
+
+    function deshabilitarDibujar() {
+
+        puedoDibujar = false;
+
+    }
+
+    //Función para delimitar el área
+    function puedeDisenharArea(xCoordenada,yCoordenada){
+
+        if ((xCoordenada >= 0 && xCoordenada < (3*tamanhoCuadrados+5)) && 
+            (yCoordenada >=0 && yCoordenada < (tamanhoCuadrados+5))) {
+
+            return false;
+
+        } else{
+
+            return true;
+
+        }
+
+    }    
+
+    function capturarMovimientoDelMouse(evento){
+
+        var x = evento.pageX - pantalla.offsetLeft;
+        var y = evento.pageY - pantalla.offsetTop;
+
+        if (puedeDisenharArea(x,y)){
+
+                dibujarCirculo(x,y,colorActual);
+
+        }
+
+    }
+
+    function seleccionarColor(evento) {
+
+        var x = evento.pageX - pantalla.offsetLeft;
+        var y = evento.pageY - pantalla.offsetTop;
+
+        //Cada condición altera el color de la variable colorActual
+
+        //Comenzamos por la condición del eje Y que es común para todas
+        if (y > yCuadrados && y < (yCuadrados + tamanhoCuadrados)) {
+
+            if(x > xRojo && x < (xRojo + tamanhoCuadrados)) {
+
+                colorActual = "red";
+                console.log(colorActual);
+
+            } else if (x > xVerde && x < (xVerde + tamanhoCuadrados)) {
+
+                colorActual = "green";
+
+            } else if(x > xAzul && x < (xAzul + tamanhoCuadrados)) {
+
+                colorActual = "blue";
+
+            }            
 
 
+        }
+
+
+    }
+
+    pantalla.onmousemove = capturarMovimientoDelMouse;
+
+    pantalla.onmousedown = habilitarDibujar;
+
+    pantalla.onmouseup = deshabilitarDibujar;
+
+    dibujarPaletaColores();
+
+    pantalla.onclick = seleccionarColor;
 
